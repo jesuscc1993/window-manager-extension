03962916840ee1370e31203a094e7bd493266cfb
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2024-02-24 18:08:15 +0100

    Fixed extension only storing past settings for the latest host.

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -57,7 +57,7 @@ const openNewWindow = () => {
         settings: {
           ...settings,
           newWindowFormData: {
-            ...((settings && settings.formData) || {}),
+            ...((settings && settings.newWindowFormData) || {}),
             [domain]: formData,
           },
         },
